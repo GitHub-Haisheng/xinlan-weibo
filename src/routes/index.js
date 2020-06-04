32fa73099067827f3e8a307808b69e16921c04bd
ant <wanghaisheng@MacBook-Pro-3.local>
2020-06-04 14:53:01 +0800

    feat:路由的演示

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,15 @@
-const router = require('koa-router')()
+const router = require('koa-router')();
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
-
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
+    title: 'Hello Koa 2!',
+  });
+});
 
 router.get('/json', async (ctx, next) => {
   ctx.body = {
-    title: 'koa2 json'
-  }
-})
+    title: 'koa2 json',
+  };
+});
 
-module.exports = router
+module.exports = router;
